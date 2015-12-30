84c02b559c98de4f7a15baa8df779c0652832c41
AbyChan <crystal.tyan@gmail.com>
2015-12-30 20:36:54 +0800

    initial commit

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+;; load dir setting file
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+(add-to-list 'load-path site-lisp-dir)
 
 
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
 
