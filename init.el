79a29dd181c69761d41b93ca7a8b9b75e62a6592
cfangwei <chenfangwei@outlook.com>
2016-01-10 15:38:08 +0800

    update

--- init.el
+++ init.el
@@ -1,125 +1,23 @@
-;; load dir setting file
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
+(setq user-emacs-directory "/home/pi/.spacemacs.d/")
+(load "/home/pi/.spacemacs.d/init.el")
 
-(add-to-list 'load-path site-lisp-dir)
+;;(load "/home/pi/.emacs.d/emacs-ligth.el")
 
-;; Add external projects to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
-
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-;;初始化透明度
-(set-frame-parameter (selected-frame) 'alpha '(92 92))
-
-;;操作不阻塞显示
-(setq redisplay-dont-pause t)
-
-
-;;高亮当前行
-(global-hl-line-mode 1)
-
-(setq custom-theme-directory (concat user-emacs-directory "themes"))
-
-(dolist
-    (path (directory-files custom-theme-directory t "\\w+"))
-  (when (file-directory-p path)
-    (add-to-list 'custom-theme-load-path path)))
-
-;; Allow pasting selection outside of Emacs
-(setq x-select-enable-clipboard t)
-
-
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
-
-;; Also auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-
-;; Show keystrokes in progress
-(setq echo-keystrokes 0.01)
-
-;; Move files to trash when deleting
-(setq delete-by-moving-to-trash t)
-
-;; Real emacs knights don't use shift to mark things
-(setq shift-select-mode nil)
-
-;; Transparently open compressed files
-(auto-compression-mode t)
-
-;; Enable syntax highlighting for older Emacsen that have it off
-(global-font-lock-mode t)
-
-
-;; UTF-8 please
-(setq locale-coding-system 'utf-8) ; pretty
-(set-terminal-coding-system 'utf-8) ; pretty
-(set-keyboard-coding-system 'utf-8) ; pretty
-(set-selection-coding-system 'utf-8) ; please
-(prefer-coding-system 'utf-8) ; with sugar on top
-
-;; Show active region
-(transient-mark-mode 1)
-(make-variable-buffer-local 'transient-mark-mode)
-(put 'transient-mark-mode 'permanent-local t)
-(setq-default transient-mark-mode t)
-
-;; Remove text in active region if inserting text
-(delete-selection-mode 1)
-
-;; Don't highlight matches with jump-char - it's distracting
-(setq jump-char-lazy-highlight-face nil)
-
-;; Always display line and column numbers
-(setq line-number-mode t)
-(setq column-number-mode t)
-
-;; Lines should be 80 characters wide, not 72
-(setq fill-column 80)
-
-;; Save a list of recent files visited. (open recent file with C-x f)
-(recentf-mode 1)
-
-;; Save minibuffer history
-(savehist-mode 1)
-(setq history-length 1000)
-
-;; Undo/redo window configuration with C-c <left>/<right>
-;;(winner-mode 1)
-
-;; Never insert tabs
-(set-default 'indent-tabs-mode nil)
-
-;; Show me empty lines after buffer end
-(set-default 'indicate-empty-lines t)
-
-;; Easily navigate sillycased words
-(global-subword-mode 1)
-
-;; 是否断行显示(open it will break)
-(setq-default truncate-lines t)
-
-
-;; Allow recursive minibuffers
-(setq enable-recursive-minibuffers t)
-
-;; Don't be so stingy on the memory, we have lots now. It's the distant future.
-(setq gc-cons-threshold 20000000)
-
-;; Sentences do not need double spaces to end. Period.
-(set-default 'sentence-end-double-space nil)
-
-;; 80 chars is a good width.
-(set-default 'fill-column 80)
-
-;; Nic says eval-expression-print-level needs to be set to nil (turned off) so
-;; that you can always see what's happening.
-(setq eval-expression-print-level nil)
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("94dfc2b6a8ac8b651f9aab66bc83bd5601f47166d86ee6d2d32de7dfb881185c" default)))
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
